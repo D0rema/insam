8915a2682c1c494a45f3fed25182a0bf43d15960
Taeung Song <Changseok Lee>
2019-11-13 16:35:43 +0900

    고양이 스테이지 추가

---
+++ insam/src/insam_re/draw_image.java
@@ -0,0 +1,188 @@
+package insam_re;
+
+import java.awt.*;
+import java.awt.image.*;
+
+import javax.swing.ImageIcon;
+
+public class draw_image {
+	Image img = new ImageIcon("images/rpg2.png").getImage();
+	Image map = new ImageIcon("images/stage1.png").getImage();//*이미지 수정
+	Image check = new ImageIcon("images/check.png").getImage();
+	Image dog = new ImageIcon("images/smdogL.png").getImage();
+	Image heart = new ImageIcon("images/life.png").getImage();
+	Image heart_x = new ImageIcon("images/life_x.png").getImage();
+	
+	obstacle dg1=new obstacle(150,155);
+	obstacle dg2=new obstacle(550,110);
+	// 위에 이미지 이름이 바로 rpg.png입니다. 이미지를 불러옵니다
+	// 더블버퍼링용 입니다.
+	Graphics gc;
+
+	private moving_test moving_test = null;
+
+	
+ 	public void paint(Graphics g, Image buffimg, ImageObserver Frame) { // 더블버퍼링을 사용합니다.
+
+		gc = buffimg.getGraphics();
+		draw_background(Frame); // 배경을 그리는 함수
+		checkmark1(Frame,20,230); // 특수효과를 그리는 함수
+		checkmark2(Frame,400,40);
+		checkmark3(Frame,750,120);
+		draw_dog1(Frame, dg1.ox,dg1.oy);
+		draw_dog2(Frame, dg2.ox,dg2.oy);
+		draw_life(Frame, moving_test.lck1,740,30);
+		draw_life(Frame, moving_test.lck2,695,30);
+		draw_life(Frame, moving_test.lck3,650,30);
+		update(g, buffimg, Frame);
+		
+
+		
+	}
+
+	public void update(Graphics g, Image buffimg, ImageObserver Frame) {
+		// 더블 버퍼링을 이용해 버퍼에 그려진것을 가져옵니다.
+		DrawImg(Frame);
+
+		g.drawImage(buffimg, 0, 0, Frame);
+	}
+
+	public void DrawImg(ImageObserver Frame) {
+		gc.setFont(new Font("Default", Font.BOLD, 20));
+		gc.drawString(Integer.toString(moving_test.cnt), 50, 50);
+		gc.drawString(Integer.toString((moving_test.playerMove) ? 1 : 0), 200, 50);
+		gc.drawString(Integer.toString(moving_test.x), 350, 50);
+		gc.drawString(Integer.toString(moving_test.y), 500, 50);
+		gc.drawString(Integer.toString(moving_test.jp1), 350, 100);
+		gc.drawString(Integer.toString(moving_test.char_lo), 500, 100);
+		// gc.drawString(Integer.toString(on_bottom),500, 150);
+		// gc.drawString(Integer.toString(on_flat),350, 150);
+
+		// 위는 단순히 무한루프 적용여부와 케릭터 방향 체크를 위해
+		// 눈으로 보면서 테스트할 용도로 쓰이는 텍스트 표출입니다.
+
+		MoveImage(img, moving_test.x, moving_test.y, 50, 75, Frame);
+		// 케릭터를 걸어가게 만들기 위해 추가로 만든 메소드 입니다.
+
+	}
+
+	public void MoveImage(Image img, int x, int y, int width, int height, ImageObserver Frame) {
+		// 케릭터 이미지, 케릭터 위치, 케릭터 크기를 받습니다.
+		// 받은 값을 이용해서 위의 이미지칩셋에서 케릭터를 잘라내
+		// 표출하도록 계산하는 메소드 입니다.
+
+		gc.setClip(x, y, width, height);
+		// 현재 좌표에서 케릭터의 크기 만큼 이미지를 잘라 그립니다.
+
+		if (moving_test.playerMove) { // 케릭터의 움직임 여부를 판단합니다.
+			if (moving_test.cnt / 10 % 4 == 0) {
+				gc.drawImage(img, x - (width * 0), y - (height * moving_test.moveStatus), Frame);
+
+			} else if (moving_test.cnt / 10 % 4 == 1) {
+				gc.drawImage(img, x - (width * 1), y - (height * moving_test.moveStatus), Frame);
+
+			} else if (moving_test.cnt / 10 % 4 == 2) {
+				gc.drawImage(img, x - (width * 2), y - (height * moving_test.moveStatus), Frame);
+
+			} else if (moving_test.cnt / 10 % 4 == 3) {
+				gc.drawImage(img, x - (width * 1), y - (height * moving_test.moveStatus), Frame);
+			}
+			// 케릭터의 방향에 따라 걸어가는 모션을 취하는
+			// 케릭터 이미지를 시간차를 이용해 순차적으로 그립니다.
+
+		} else {
+			gc.drawImage(img, x - (width * 1), y - (height * moving_test.moveStatus), Frame);
+			// 케릭터가 움직이지 않으면 정지한 케릭터를 그립니다.
+
+		}
+	}
+	public void draw_background(ImageObserver Frame) {
+		gc.clearRect(0, 0, 800, 600);
+		gc.drawImage(map, 0, 0, Frame);
+	}
+	public void draw_dog1(ImageObserver Frame, int dx, int dy) {
+		if ((dx-10<=moving_test.x && dx+30>=moving_test.x) && 
+				(dy-10<=moving_test.y && dy+10>=moving_test.y)) {
+			if(moving_test.x<=dx)moving_test.x=moving_test.x-50;
+			else 
+				moving_test.x=moving_test.x+50;
+			
+			if(moving_test.lck1)
+				moving_test.lck1=false;
+			else if(moving_test.lck2)
+				moving_test.lck2=false;
+			else moving_test.lck3=false;
+		}
+		if((moving_test.cnt/110)%2==0) {
+			dg1.ox=dg1.ox+2;
+		}
+		else
+			dg1.ox=dg1.ox-2;
+		gc.drawImage(dog, dx, dy, Frame);
+	}
+	public void draw_dog2(ImageObserver Frame, int dx, int dy) {
+		if ((dx-10<=moving_test.x && dx+30>=moving_test.x) && 
+				(dy-10<=moving_test.y && dy+10>=moving_test.y)) {
+			if(moving_test.x<=dx)moving_test.x=moving_test.x-50;
+			else
+				moving_test.x=moving_test.x+50;
+			
+			if(moving_test.lck1)
+				moving_test.lck1=false;
+			else if(moving_test.lck2)
+				moving_test.lck2=false;
+			else moving_test.lck3=false;
+		}
+		if((moving_test.cnt/70)%2==0) {
+			dg2.ox=dg2.ox+3;
+		}
+		else
+			
+			dg2.ox=dg2.ox-3;
+		gc.drawImage(dog, dx, dy, Frame);
+	}
+
+	public void checkmark1(ImageObserver Frame,int cx,int cy) {
+		if ((cx-10<=moving_test.x && cx+10>=moving_test.x) && 
+				(cy-10<=moving_test.y && cy+10>=moving_test.y)) {
+			moving_test.ck1 = false;
+
+		}
+
+		if (moving_test.ck1 == true) {
+			gc.drawImage(check, cx, cy, Frame);
+		}
+	}
+	
+	public void checkmark2(ImageObserver Frame,int cx,int cy) {
+		if ((cx-15<=moving_test.x && cx+15>=moving_test.x) && 
+				(cy-15<=moving_test.y && cy+15>=moving_test.y)) {
+			moving_test.ck2 = false;
+
+		}
+
+		if (moving_test.ck2 == true) {
+			gc.drawImage(check, cx, cy, Frame);
+		}
+	}
+	
+	public void checkmark3(ImageObserver Frame,int cx,int cy) {
+		if ((cx-10<=moving_test.x && cx+10>=moving_test.x) && 
+				(cy-10<=moving_test.y && cy+10>=moving_test.y)) {
+			moving_test.ck3 = false;
+			
+
+		}
+
+		if (moving_test.ck3 == true) {
+			gc.drawImage(check, cx, cy, Frame);
+		}
+	}
+	public void draw_life(ImageObserver Frame,boolean lck , int lx, int ly) {
+		if(lck) {
+			gc.drawImage(heart,lx, ly,Frame );
+		}
+		else
+			gc.drawImage(heart_x,lx,ly,Frame);
+	}
+}
---
+++ insam/src/insam_re/moving_test.java
@@ -0,0 +1,162 @@
+package insam_re;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.image.ImageObserver;
+
+public class moving_test {
+	public static void main(String[] ar) {
+		RpgGame_frame rg = new RpgGame_frame();
+	}
+
+	public static boolean playerMove = false;
+	public static int x, y; // 케릭터의 현재 좌표를 받을 변수
+	public static int g_x, g_y; // 중력을 체크하기 위한 좌표
+	public static int cnt; // 무한 루프를 카운터 하기 위한 변수
+	public static int moveStatus; // 케릭터가 어디를 바라보는지 방향을 받을 변수
+	public static int jp1, jp2; // 점프 cnt를 계산하기 위한 값
+	public static boolean try_jump = false;
+	public static boolean ck1 = true, ck2 = true, ck3 = true;
+	public static boolean lck1 = true, lck2 = true, lck3 = true;
+	public static int not_key = 0;
+	public static int char_lo = 0;
+//키가 입력받아서 캐릭터가 움직이는 상황에는 true로 바뀜
+
+}
+
+class RpgGame_frame extends JFrame implements Runnable, KeyListener {
+
+	key k1 = new key();
+
+	draw_image d1 = new draw_image();
+	draw_image d2 = new draw_image();
+	check_wall w1 = new check_wall();
+	Ladder l = new Ladder();
+	// puzzle_master z1 = new puzzle_master();
+
+	Image buffimg;
+	public ImageObserver Frame = this;
+
+	public void init() {
+		moving_test.x = 20;// 시작할때 캐릭터의 좌표
+		moving_test.y = 400;// 시작할때 캐릭터의 좌표
+		moving_test.moveStatus = 1;
+//케릭터가 시작할때 바라보는 방향은 아래쪽입니다.
+// 0 : 위쪽, 1 : 오른쪽, 2 : 아래쪽, 3 : 왼쪽
+
+	}
+
+	RpgGame_frame() {
+		setTitle("테스트");
+		setSize(800, 600);
+		init();
+		start();
+
+		Dimension screen = tk.getScreenSize();
+
+		int xpos = (int) (screen.getWidth() / 2 - getWidth() / 2);
+		int ypos = (int) (screen.getHeight() / 2 - getHeight() / 2);
+		setLocation(xpos, ypos);
+		setResizable(false);
+		setVisible(true);
+
+	}
+
+	public void keyTyped(KeyEvent e) {
+		k1.keyTyped(e);
+	}
+
+	public void keyReleased(KeyEvent e) {
+		k1.keyReleased(e);
+	}
+
+	public void keyPressed(KeyEvent e) {
+		k1.keyPressed(e);
+	}
+
+	public void paint(Graphics g) {
+		buffimg = createImage(800, 600);
+		d1.paint(g, buffimg, Frame);
+	}
+
+	public void update(Graphics g) {
+		d1.update(g, buffimg, Frame);
+	}
+
+	public void DrawImg() {
+		d1.DrawImg(Frame);
+	}
+
+	public void MoveImage(Image img, int x, int y, int width, int height) {
+		d1.MoveImage(img, x, y, width, height, Frame);
+	}
+
+	public void draw_background() {
+		d1.draw_background(Frame);
+	}
+
+	/*
+	 * public void checkmark() { d1.checkmark(Frame); }
+	 */
+
+	public void in_flat() {
+		w1.in_flat();
+	}
+
+	public void is_bottom() {
+		w1.is_bottom();
+	}
+
+	public void on_wall() {
+		w1.on_wall();
+	}
+
+	public boolean ck_check() {
+		if (moving_test.ck1 || moving_test.ck2 || moving_test.ck3)
+			return false;
+		else
+			return true;
+	}
+
+	public boolean lck_check() {
+		if (moving_test.lck3)
+			return false;
+		else
+			return true;
+	}
+	/*
+	 * public void puzzle_master() { z1.find_load(); }
+	 */
+
+	Toolkit tk = Toolkit.getDefaultToolkit();
+
+	Thread th;
+
+	public void start() { // 기본적인 명령처리
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.addKeyListener(this);
+		th = new Thread(this);
+		th.start();
+	}
+
+	public void run() { // 스레드 메소드, 무한 루프
+		while (true) {
+			try {
+				k1.keyProcess();
+				repaint();
+				on_wall();
+//in_flat(); // 발판을 체크할수있는 함수 check_wall()로 통합 할수도있음
+				Thread.sleep(20);
+				moving_test.cnt++;
+				l.on_Ladder();
+//is_bottom(); // 중력과 바닥을 체크하는 함수 ,아래 w1. check_wall()로 통합 할수도있음
+				w1.wall_Check();
+				if (lck_check()) {
+				}
+				// puzzle_master();
+			} catch (Exception e) {
+			}
+		}
+	}
+}
